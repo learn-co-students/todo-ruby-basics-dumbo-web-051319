e4fe760eda0328e7bc7c0f15d43ff8d8abdea354
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-19 19:17:36 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,24 +1,31 @@
 def division(num1, num2)
- 
+#failure because it needed the first number to be divided by the second number 
+  num1/num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(value_here)
+  #failure because the argument had no value
+  variable=value_here
 end
 
-def argue
-  
+def argue(argument_here)
+  #failure because there was no argument
+  argument_here
 end
 
-def greeting
-
+def greeting(greeting,name)
+  #failure because it was expecting two arguments(greeting and name)
+  puts "#{greeting}, #{name}"
 end
 
 def return_a_value
-
+#failure because it wanted to return the phrase 'Nice'
+  return "Nice"
 end
 
 def last_evaluated_value
+  #failre because expected to return phrase 'expert'
+  return "expert"
 
 end
 
